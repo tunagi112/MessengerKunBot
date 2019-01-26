86f29f491ebba4ac8d3cd0b29f67a411a7263ce2
tunagi112 <tunagi112@gmail.com>
2019-01-26 16:31:00 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+
+LINE_CHANNEL_ACCESS_TOKEN = "fqlgBVLmeeArUi03DQDKL5ucc80VOvJUMeaRTskfdK3DN9lrDHHkDKxA6Be8p32RBw2agZL90uQrsn2cRusqL0zG/TaFi5B/jZnGwRwPYEfNPD3TqD7I24xh5Bj9cCwOKTZ/po/0+kW8V5xaiGXeRAdB04t89/1O/w1cDnyilFU="
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+
+
+def main():
+    user_id = "itaku21"
+
+    message = TextSendMessage(text=f"こんにちは\n\n"
+                                    f"おはよう")
+    line_bot_api.push_message(user_id,messages=message)
+
+    if __name__ == "__main__":
+        main()
